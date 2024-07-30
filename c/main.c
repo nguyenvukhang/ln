b2d003a5a5a31c2997dcc1a607f8d12025ce9cc3
nguyenvukhang <brew4k@gmail.com>
2024-07-30 22:42:01 +0800

    Various improvements

--- c/main.c
+++ c/main.c
@@ -4,32 +4,37 @@
 #include <time.h>
 #include <unistd.h>
 
-#define print(text, len) write(STDOUT_FILENO, text, len)
-#define SEP " :_|"
-#define SEP_L (sizeof(SEP) - 1);
+#define print(text, len) fwrite(text, sizeof(char), len, stdout)
+#define Print(text) print(text, sizeof(text) - 1)
+#define SEP ",.;:"
+#define SEP_L (sizeof(SEP) - 1)
 #define BUF_SIZE 0x200
 
-#define NORMAL() print("\x1b[0m", 4)
-#define YELLOW() print("\x1b[33m", 5)
-#define DARK_GRAY() print("\x1b[38;5;241m", 11)
-#define LIGHT_GRAY() print("\x1b[38;5;246m", 11)
+// surrounding git's colorful refs.
+#define N_PRE_REFS sizeof("\x1b[m \x1b[33m")
+#define N_POST_REFS sizeof("\x1b[m\x1b[33m)\x1b[m")
+
+#define RESET "\x1b[m"
+
+#define DARK_GRAY "\x1b[38;5;240m"
+#define LIGHT_GRAY "\x1b[38;5;246m"
 
 static int t; // time since unix epoch (in secs)
 
-void print_refs(const char *x, int len) {
-  LIGHT_GRAY();
-  print("(", 1);
-  for (int i = 0; i < len; i++) {
-    if (strncmp(&x[i], "origin", 6) == 0) {
+void print_refs(char *x, int len) {
+  Print(DARK_GRAY "{");
+  for (int i = 0; i < len; i++, x++) {
+    if (strncmp(x, "origin", 6) == 0) {
       i += 6;
-      print("*", 1);
+      x += 6;
+      Print("*");
       continue;
+    } else if (strncmp(x, "\x1b[33m", 5) == 0) {
+      x[3] = '7'; // yellow -> gray.
     }
-    print(&x[i], 1);
+    print(x, 1);
   }
-  LIGHT_GRAY();
-  print(")", 1);
-  NORMAL();
+  Print(DARK_GRAY "}");
 }
 
 int print_time(const char *time_str) {
@@ -38,14 +43,14 @@ int print_time(const char *time_str) {
 #define send(l)                                                                \
   snprintf(buf, 8, "%d", n);                                                   \
   print(buf, strlen(buf));                                                     \
-  return print(l, 1);
+  return Print(l);
   // clang-format off
-  if (n < 60)         { send("s"); }
+  if  (n        < 60) { send("s"); }
   if ((n /= 60) < 60) { send("m"); }
   if ((n /= 60) < 24) { send("h"); }
   if ((n /= 24) < 7)  { send("d"); }
+       n /=  7;         send("w");
   // clang-format on
-  send("w");
 #undef send
 }
 
@@ -56,39 +61,30 @@ int send_line(const char *buf) {
     return print(buf, strlen(buf));
   hash += SEP_L;
 
-  const char *refs = strstr(hash, SEP) + SEP_L;
-  const char *comment = strstr(refs, SEP) + SEP_L;
-  const char *time_str = strstr(comment, SEP) + SEP_L;
+  char *refs = strstr(hash, SEP) + SEP_L;
+  char *comment = strstr(refs, SEP) + SEP_L;
+  char *time_str = strstr(comment, SEP) + SEP_L;
 
-  int time = strtol(time_str, NULL, 10);
+  int time = strtol(time_str, NULL, 10), refs_l;
 
-  int graph_l = hash - buf - SEP_L;
-  int hash_l = refs - hash - SEP_L;
-  int refs_l = comment - refs - SEP_L;
-  int comment_l = time_str - comment - SEP_L;
+  print(graph, hash - graph - SEP_L);
+  Print("\x1b[33m");
+  print(hash, refs - hash - SEP_L);
+  Print(" ");
 
-  print(graph, graph_l);
-  YELLOW();
-  print(hash, hash_l + 1);
-  NORMAL();
-
-  if (refs_l > 6) {
-    print_refs(refs + 10, refs_l - 25);
-    print(" ", 1);
+  if ((refs_l = comment - refs - SEP_L) > 8) {
+    print_refs(refs + N_PRE_REFS, refs_l - N_PRE_REFS - N_POST_REFS + 1);
+    Print(" ");
   }
+  Print(RESET);
 
-  print(comment, comment_l);
-  DARK_GRAY();
-  print(" (", 2);
-  LIGHT_GRAY();
+  print(comment, time_str - comment - SEP_L);
+  Print(DARK_GRAY " (" LIGHT_GRAY);
   print_time(time_str);
-  DARK_GRAY();
-  print(")", 1);
-  NORMAL();
-  return print("\n", 1);
+  return Print(DARK_GRAY ")" RESET "\n");
 }
 
-int main(int argc, const char **argv) {
+int main(const int argc, const char **argv) {
   t = time(NULL);
 
   int p[2]; // [read, write]
@@ -100,16 +96,18 @@ int main(int argc, const char **argv) {
   // Start a fork for `git` where it writes to `p[1]`.
   if (fork() == 0) {
     close(p[0]);
-    const char *args[argc + 1 + 4];
-    args[0] = "git";
-    args[1] = "log";
-    for (int i = 1; i < argc; i++)
-      args[i + 1] = argv[i];
-    args[++argc] = "--color=always";
-    args[++argc] = "--graph";
-    args[++argc] =
-        "--format=" SEP "%h" SEP "%C(auto)%d%Creset" SEP "%s" SEP "%at";
-    args[++argc] = NULL;
+    const char *args[argc + 8];
+    int i = 0;
+#define ARG(v) args[i++] = v
+    ARG("git");
+    ARG("log");
+    for (int j = 1; j < argc; j++, i++)
+      args[i] = argv[j];
+    ARG("--color=always");
+    ARG("--graph");
+    ARG("--format=" SEP "%h" SEP "%C(auto)%d" SEP "%s" SEP "%at");
+    ARG(NULL);
+#undef ARG
     dup2(p[1], STDOUT_FILENO);
     execvp("git", (char *const *)args);
   }
